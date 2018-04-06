b0d3433f84969b61a6bc26fcd15a613882bb65f9
sosim13 <sosim13p@gmail.com>
2018-04-06 16:53:22 +0900

    321

--- ui/js/common.js
+++ ui/js/common.js
@@ -27,43 +27,14 @@ function css(selector, property, value) {
     }
 }
 
-hideElement('banner_left');
-hideElement('t_left');
-hideElement('mb_l');
+hideElement('crumbs');
+hideElement('divpop_50');
+hideElement('divpop_51');
+hideElement('divpop_52');
+hideElement('divpop_53');
 
 
 
-
-
-css("body", "background-color", "rgba(255, 255, 255, 0.01)");
-css("#listWrap.index h2 span", "background-color", "rgba(255, 255, 255, 0.01)");
-css("body", "font-size", "24px");
-css("a:link", "font-size", "24px");
-css("a:visited", "font-size", "24px");
-css("a:hover", "font-size", "24px");
-css("tr, td, th", "font-size", "24px");
-css("tr, td, th", "font-size", "24px");
-css("table.bbslist td#tdate", "font-size", "1px");
-
-css("table.bbslist", "width", "1000px");
-css("#t_right", "margin", "0 0 0 0");
-css("div#banner_box", "width", "1000px");
-css("div#banner_box", "margin", "0 0 0 0");
-
-// 읽기 게시판
-css("table.bbslist", "width", "1000px");
-css("#t_right", "margin", "0 0 0 0");
-css("table.bbslist", "width", "1000px");
-css("table.bbslist", "margin", "10px 0px 10px 0px");
-css("table.reply_box", "width", "1000px");
-css("table.reply_box", "margin", "10px 0px 10px 0px");
-css("div#banner_box", "width", "1000px");
-css("div#banner_box", "margin", "10px 0px 10px 0px");
-css("table.rp_box", "width", "1000px");
-css("table.rp_box", "margin", "10px 0px 10px 0px");
-css("table.rp_box2", "width", "1000px");
-css("table.rp_box2", "margin", "10px 0px 10px 0px");
-css("div#con_text", "font-size", "50px");
 
 
 //css("input", "background-color", "rgba(255, 255, 255, 0.01)");
