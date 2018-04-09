59d439ed5315d89ef0b6177e0fcf731bf82aa518
sosim13 <sosim13p@gmail.com>
2018-04-09 16:13:27 +0900

    321

--- ui/js/common.js
+++ ui/js/common.js
@@ -42,9 +42,14 @@ css("#top .search2 li", "display", "none");
 css(".container", "margin", "0px");
 css(".container", "width", "690px");
 
+var customerId = "1";
+function kkk(){
 
-alert($(body));
+	customerId.val("2");
+}
 
+kkk();
+alert(customerId);
 
 //css("body", "background-color", "rgba(255, 255, 255, 0.01)");
 //css("#listWrap.index h2 span", "background-color", "rgba(255, 255, 255, 0.01)");
